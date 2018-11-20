37d30b93565a301c2041815e82604d81b5cff7c8
Ilya Labacheuski <ilya_labacheuski@epam.com>
2018-11-20 18:03:40 +0300

    Save work

---
+++ main.py
@@ -0,0 +1,12 @@
+# x = 3 
+# while x < 13:
+#     print(x)
+#     x += 3
+
+# for e in range(5):
+#     print("hell")
+# for animal in ["корова", "мышь"]:
+#     print("{} - это животные".format(animal))
+while True:
+    print("som")
+    break
