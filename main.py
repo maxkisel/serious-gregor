e9bd91f538c5c2a38b968517d4c667c5b20e6768
Maxim <max.kisel.2006@gmail.com>
2018-12-11 18:36:30 +0300

    lesson-10

---
+++ main.py
@@ -0,0 +1,19 @@
+def tyhg():
+    print("eujs")
+
+tyhg()
+tyhg()
+def add(x, y):    
+    return x + y
+
+print(add(2,3))
+
+def keyword_args(**kwargs):
+    return kwargs
+
+print(keyword_args(big = "foot", loch = "ness"))
+
+def setX(mum):
+    X = mum
+    print (X)
+    
